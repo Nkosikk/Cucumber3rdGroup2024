2f67980365f2370becebed9e6cdac8ab827be973
sgwasira <sgwasira@yahoo.com>
2024-08-13 19:31:01 +0200

    created the verifyLoginPage method

--- Cucumber3rdGroup2024/src/test/java/pages/LoginPage.java
+++ Cucumber3rdGroup2024/src/test/java/pages/LoginPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
 public class LoginPage {
 
@@ -11,12 +12,21 @@ public class LoginPage {
     @FindBy(id = "user-name")
     WebElement username_id;
 
+    @FindBy(xpath = "//span[@class='title'][contains(.,'Products')]")
+    WebElement productTitle_xpath;
+
+
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void enterUsername(String username) {
         username_id.sendKeys(username);
+    }
+
+
+    public  void verifyLoginPage(){
+        Assert.assertEquals(productTitle_xpath.getText(), "Products");
     }
 
     //ToDo create method to enter password - Thabiso

4b3d1468bbf1f4c475a4123b5c9326121ee328d9
Tutja <Consciousness.Tutja@kerridgecs.com>
2024-08-14 16:00:15 +0200

    Added a method to add a product to cart

--- Cucumber3rdGroup2024/src/test/java/pages/InventoryPage.java
+++ Cucumber3rdGroup2024/src/test/java/pages/InventoryPage.java
@@ -12,11 +12,20 @@ public class InventoryPage {
     @FindBy(xpath = "//span[@class='title'][contains(.,'Products')]")
     WebElement productTitle_xpath;
 
+    @FindBy(id ="add-to-cart-sauce-labs-backpack")
+    WebElement addProductToCart_id;
+
+
     public InventoryPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void verifyProductPage() {
         Assert.assertEquals(productTitle_xpath.getText(), "Products");
+    }
+
+    public void addProductToCart(){
+        addProductToCart_id.click();
+
     }
 }
